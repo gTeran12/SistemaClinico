40fd7cd681cab9bf93e0c66d8a78a56386a38872
bryansegovia <bsegovia@espol.edu.ec>
2020-11-24 23:39:01 -0500

    Solicitar Cita

--- TallerSistemaCl/src/Datos/Cita.java
+++ TallerSistemaCl/src/Datos/Cita.java
@@ -13,8 +13,53 @@ public class Cita
     protected Paciente paciente;
     protected Doctor doctor;
     
+    
+    
     public boolean realizarPago(Pago pago)
     {
         throw new UnsupportedOperationException("Not supported yet."); 
     }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public boolean isPagada() {
+        return pagada;
+    }
+
+    public void setPagada(boolean pagada) {
+        this.pagada = pagada;
+    }
+
+    public String getRegistradoPor() {
+        return registradoPor;
+    }
+
+    public void setRegistradoPor(String registradoPor) {
+        this.registradoPor = registradoPor;
+    }
+
+    public Paciente getPaciente() {
+        return paciente;
+    }
+
+    public void setPaciente(Paciente paciente) {
+        this.paciente = paciente;
+    }
+
+    public Doctor getDoctor() {
+        return doctor;
+    }
+
+    public void setDoctor(Doctor doctor) {
+        this.doctor = doctor;
+    }
+    
+    
+    
 }
--- TallerSistemaCl/src/Usuarios/Paciente.java
+++ TallerSistemaCl/src/Usuarios/Paciente.java
@@ -1,9 +1,11 @@
 package Usuarios;
 
+import Accion.EnviarCorreo;
 import Datos.Cita;
 import Datos.HistoriaClinica;
 import Datos.Receta;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class Paciente extends Persona
 {
@@ -21,6 +23,19 @@ public class Paciente extends Persona
     
     public boolean solicitarCita()
     {
-        throw new UnsupportedOperationException("Not supported yet.");
+        cita= new Cita();  
+        cita.setFecha(new Date());
+        cita.setPagada(true);
+        cita.setRegistradoPor("Juan Piguave");
+        cita.setPaciente(this);
+        cita.setDoctor(new Doctor());
+        
+        EnviarCorreo mail = new EnviarCorreo("bsegovia@espol.edu.ec", "Confirmacion de Cita", "Fecha: "+ cita.getFecha().toString()
+                + "\nRegistrada Por: "+ cita.getRegistradoPor()+ "\nPaciente: "+ this.nombre +" "+this.apellido
+                + "\nDoctor: " +cita.getDoctor().apellido, 0);
+        mail.ejecutarEnviarCorreo();
+        System.out.println("Se ha enviado la confirmacion de la cita a su correo electronico");
+        return true;
+        
     }
 }
